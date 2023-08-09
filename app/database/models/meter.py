ce2836e88e5bbbecd5735aa267c2d5f72999cc0e
manjaro | stijn <stijnvdpouw@gmail.com>
2023-08-09 19:53:58 +0200

    make source id unique instd on ean

--- app/database/models/meter.py
+++ app/database/models/meter.py
@@ -11,16 +11,16 @@ class MeterModel(BaseModel):
     name: Mapped[str] = mapped_column()
     commodity: Mapped[str] = mapped_column()
     status: Mapped[str] = mapped_column()
-    ean: Mapped[str] = mapped_column(unique=True)
-    source_id: Mapped[str] = mapped_column()
+    ean: Mapped[str] = mapped_column()
+    source_id: Mapped[str] = mapped_column(unique=True)
     installed_at: Mapped[datetime] = mapped_column(nullable=True)
     installation_id: Mapped[int] = mapped_column(
         ForeignKey("installation.id"), index=True
     )
     channels: Mapped[list[ChannelModel]] = relationship(
-    "ChannelModel",
-    backref="meter",
-    cascade="all, delete-orphan",
+        "ChannelModel",
+        backref="meter",
+        cascade="all, delete-orphan",
     )
 
     # channels: Mapped[list[ChannelModel]] = relationship(backref="meter")
